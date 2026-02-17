711466f634214c6da9cdb34395438b55c9322524
sajirhtml <sajirhtml@gmail.com>
2026-02-17 22:47:41 +0600

    test3

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,42 @@
-import React, { use } from 'react';
-import { NavLink } from 'react-router';
-const categoryPromise = fetch("https://fakestoreapi.com/products/categories").then(res=>res.json());
+import React, { use } from "react";
+import { NavLink } from "react-router";
+const categoryPromise = fetch(
+  "https://fakestoreapi.com/products/categories",
+).then((res) => res.json());
 
 const Categories = () => {
-    const categories = use(categoryPromise);
-    return (
-        <div className='text-center '>
-            <h1 className='text-3xl my-5 font-semibold'>Our Products</h1>
-            <div className='flex flex-wrap justify-center items-center gap-2 my-5'>
-                <NavLink className='btn btn-ghost btn-outline rounded-4xl'>All</NavLink>
-                {
-                    categories.map(category =>(
-                        <NavLink className='btn btn-ghost btn-outline rounded-4xl' to={`/products/${category}`}>{category}</NavLink>
-                    ))
-                }
-            </div>
-        </div>
-    );
+  const categories = use(categoryPromise);
+  return (
+    <div className="text-center capitalize">
+      <h1 className="text-3xl my-5 font-semibold">Our Products</h1>
+      <div className="flex flex-wrap justify-center items-center gap-2 my-5">
+        <NavLink
+          end
+          to="/products"
+          className={({ isActive }) =>
+            isActive
+              ? "px-4 py-2 rounded-4xl border border-gray-300 bg-indigo-600 text-white capitalize"
+              : "px-4 py-2 rounded-4xl border border-gray-300 text-gray-700 hover:text-indigo-600 transition-colors capitalize"
+          }
+        >
+          All
+        </NavLink>
+        {categories.map((category) => (
+          <NavLink
+            key={category}
+            to={`/products/${category}`}
+            className={({ isActive }) =>
+              isActive
+                ? "px-4 py-2 rounded-4xl border border-gray-300 bg-indigo-600 text-white capitalize"
+                : "px-4 py-2 rounded-4xl border border-gray-300 text-gray-700 hover:text-indigo-600 transition-colors capitalize"
+            }
+          >
+            {category}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
